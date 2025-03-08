78dc9eb53cdf91da17dde7ff3d74278ca7a5ae84
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-03-08 00:39:47 +0000

    Added constants and libraries in new file enneagonPrismCalc.cpp

---
+++ enneagonPrismCalc.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2025. Joseph Wondimagnehu. All rights reserved.
+//
+// Created by: Joseph Wondimagnehu
+// Date: Mar. 7, 2025
+/* This program allows the user to calculate the volume,
+surface area, lateral surface area, and base area of
+an enneagonal prism by asking them for the base length
+(edge) and height. */
+
+// Include iostream, string, and cstdio libraries.
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+// Run the main function.
+int main() {
+
+    // Declare all constants as containing a string.
+    printf("hi");
+    // For escape codes.
+    const std::string LIGHT_GREEN = "\033[1;32m",
+    LIGHT_CYAN = "\033[1;36m",
+    LIGHT_PURPLE = "\033[1;35m",
+    LIGHT_RED = "\033[1;31m",
+    DARK_GRAY = "\033[1;30m",
+    LIGHT_BLUE = "\033[1;34m",
+    LIGHT_YELLOW = "\033[1;33m",
+    WHITE = "\033[0m", 
+
+    // For some input error detections.
+    ACCEPTED_CHOICES[4] = {"volume", "surface area", 
+    "lateral surface area", "base area"},
+    ACCEPTED_UNITS[4] = {"mm", "cm", "in", "ft"};
+
+    // Greet the user with a light green text.
+
+}
