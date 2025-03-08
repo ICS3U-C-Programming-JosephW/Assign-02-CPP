93fe45abf398736abe2211985c68c48ba64107bd
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-03-08 11:31:48 +0000

    Removed everything except header comments in enneagonPrismCalc.cpp to start over again.

--- enneagonPrismCalc.cpp
+++ enneagonPrismCalc.cpp
@@ -5,5 +5,4 @@
 /* This program allows the user to calculate the volume,
 surface area, lateral surface area, and base area of
 an enneagonal prism by asking them for the base length
-(edge) and height. */
-
+(edge) and height. */ 
