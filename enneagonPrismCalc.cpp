e344478375d0b57ddcb2985d5c8adcdf9d10ccf0
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-03-08 01:50:55 +0000

    Working on try catch system.

--- enneagonPrismCalc.cpp
+++ enneagonPrismCalc.cpp
@@ -10,28 +10,57 @@ an enneagonal prism by asking them for the base length
 // Include iostream, string, and cstdio libraries.
 #include <iostream>
 #include <string>
-#include <cstdio>
+#include <algorithm>
+
+// Declare constants for some input error detections.
+const std::string ACCEPTED_CHOICES[4] = {"volume", "surface area", 
+"lateral surface area", "base area"}, 
+ACCEPTED_UNITS[4] = {"mm", "cm", "in", "ft"};
+    
+// Get and check the user's choice for calculation.
+std::string initChoiceInput() {
+    // Construct an infinite loop.
+    while (true) {
+        // Try to get a correct input from the user.
+        try {
+            // Declare the string variable.
+            std::string targetCalculation;
+            std::string findChoice;
+            bool validChoice;
+
+            // Get what the user wants to calculate for the enneagonal prism.
+            std::cout << "\n\033[1;35mWhat do you want to calculate? Again, choices are:" 
+" volume, surface area, lateral surface area, or base area."
+" Enter only one of these, in all lowercase.\033[0m\n";
+            std::cin >> targetCalculation;
+
+            for (int i = 0; i < 4; i++) {
+               if (targetCalculation == ACCEPTED_CHOICES[i]) {
+                   findChoice = ACCEPTED_CHOICES[i];
+                   validChoice = true;
+               }
+            };
+
+            // Checks if the user did not input a valid choice.
+            if (validChoice == true) {
+                throw(targetCalculation);
+            };
+        } 
+        
+        catch (std::string d) {
+            std::cout << "No.\n";
+        }
+    }
+}
 
 // Run the main function.
 int main() {
 
-    // Declare all constants as containing a string.
-    printf("hi");
-    // For escape codes.
-    const std::string LIGHT_GREEN = "\033[1;32m",
-    LIGHT_CYAN = "\033[1;36m",
-    LIGHT_PURPLE = "\033[1;35m",
-    LIGHT_RED = "\033[1;31m",
-    DARK_GRAY = "\033[1;30m",
-    LIGHT_BLUE = "\033[1;34m",
-    LIGHT_YELLOW = "\033[1;33m",
-    WHITE = "\033[0m", 
+    // Greet the user with a light purple text.
+    std::cout << "\n\033[1;35mHello, user! This is a program "
+"designed to calculate the volume, "
+"surface area, lateral surface area, and base area of an "
+"enneagonal prism!\033[0m\n";
 
-    // For some input error detections.
-    ACCEPTED_CHOICES[4] = {"volume", "surface area", 
-    "lateral surface area", "base area"},
-    ACCEPTED_UNITS[4] = {"mm", "cm", "in", "ft"};
-
-    // Greet the user with a light green text.
-
+    initChoiceInput();
 }
